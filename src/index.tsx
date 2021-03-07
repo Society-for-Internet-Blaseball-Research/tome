20acff51022f428c39b238e0d21c024992eaf422
iliana etaoin <iliana@buttslol.net>
2021-03-07 00:16:28 -0800

    Update README.md

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import { setup as setupColorScheme } from "./lib/color-scheme";
 
+// TODO button to override media query
 setupColorScheme();
 
 ReactDOM.render(
