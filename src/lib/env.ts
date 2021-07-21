48bfb7bd9ac085fd778f3c1f5855668cf6477ffa
iliana etaoin <iliana@buttslol.net>
2021-07-21 13:31:48 -0700

    update cors url

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,2 +1,2 @@
 // TODO pretty sure this should just be in .env or something? unsure lol
-export const BLASEBALL_ROOT = "https://cors-proxy.blaseball-reference.com";
+export const BLASEBALL_ROOT = "https://api.sibr.dev/corsmechanics/www.blaseball.com/";
